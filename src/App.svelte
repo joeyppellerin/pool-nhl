dff8215569072ff0a1c55a38592c359a15b7d9e3
Joey Pelletier <joeyp.pellerin@gmail.com>
2021-10-02 11:54:51 -0400

    Add tailwindcss to the project

--- src/App.svelte
+++ src/App.svelte
@@ -3,28 +3,22 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div class="text-center p-4 max-w-xs mx-auto sm:max-w-none">
+    <h1 class="text-red-500 text-6xl uppercase leading-loose font-thin">
+      Hello {name}!
+    </h1>
+    <p class="text-gray-700">
+      Visit the
+      <a class="text-blue-600 hover:underline" href="https://svelte.dev/tutorial">
+        Svelte tutorial
+      </a>
+      to learn how to build Svelte apps.
+    </p>
+  </div>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+<style lang="postcss" global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
 </style>
